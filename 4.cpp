d5064febd2b04b7223aef97d6af2101144462a6d
mashka-sav <164873696+mashka-sav@users.noreply.github.com>
2024-03-25 14:42:27 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int itc_multi_num(long long number){
+int a = 1;
+if (number == 0){
+    a = 0;
+}
+if (number < 0){
+    number = number* -1;
+}
+while (number != 0){
+    a = a*number%10;
+    number = number / 10;
+}
+return a;
+}
