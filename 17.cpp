a6d9b5cc74fdc7ffdd48ed6f7b39cdcde35de2a2
mashka-sav <164873696+mashka-sav@users.noreply.github.com>
2024-04-01 15:03:40 +0300

    17.cpp

--- 17.cpp
+++ 17.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int itc_covert_num(long long number, int ss){
-int a = 0;
+long long a = 0;
 long long ch = 10;
-long long num;
+long long num = 0;
 while(number > 0){
     a = number%ss;
     number = number / ss;
