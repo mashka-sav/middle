28bc69d902307caefe3394a9f7da0574a56f45af
mashka-sav <164873696+mashka-sav@users.noreply.github.com>
2024-04-01 14:42:34 +0300

    middle.h

--- middle.h
+++ middle.h
@@ -16,6 +16,7 @@ int itc_second_simple_max_num(long long number);
 long long itc_bin_num(long long number);
 long long itc_oct_num(long long number);
 int itc_rev_bin_num(long long number);
+int itc_rev_oct_num(long long number);
 
 #endif // MIDDLE_H_INCLUDED
 
