fe82e1013e92c44ba7a44dedce8d6e14d040290c
mashka-sav <164873696+mashka-sav@users.noreply.github.com>
2024-04-01 13:40:43 +0300

    10.cpp

--- 10.cpp
+++ 10.cpp
@@ -5,20 +5,12 @@ using namespace std;
 int itc_mirror_count(long long number){
 long long a = 1;
 int ch = 0;
-if(number > 0){
-while (a < number){
-    if(itc_mirror_num(a) == 1){
+while (a <= number){
+    if(itc_mirror_num(a)){
         ch++;
     }
     a++;
 }
-} else{
-while (a > number){
-    if(itc_mirror_num(a) == 1){
-        ch++;
-    }
-    a--;
-}
+
 return ch;
-}
 }
