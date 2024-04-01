0106cdc2a1bff2d43ada6fe5e3cf6c3915b421aa
mashka-sav <164873696+mashka-sav@users.noreply.github.com>
2024-04-01 13:59:11 +0300

    14.cpp

--- 14.cpp
+++ 14.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 long long itc_oct_num(long long number){
-int a = 0;
+long long a = 0;
 long long ch = 10;
-long long num;
+long long num = 0;
 while(number > 0){
     a = number%8;
     number = number / 8;
