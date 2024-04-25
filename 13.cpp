273dff849025d5146a95c88170bb94eefa77a1cd
Juli-ute <161311679+Juli-ute@users.noreply.github.com>
2024-04-25 14:28:18 +0300

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
-long long itc_bin_num(long long number){
+
+long long itc_bin_num(long long ch){
+long long j = 0;
+long long jj = 0;
+int y = 0;
+long long result = 0;
 long long a = 0;
-long long ch = 10;
-long long num = 0;
-while(number > 0){
-    a = number%2;
-    number = number / 2;
-    num = ch*a + num;
-    ch = ch*10;
+
+if(ch % 2 == 0){
+ y = 1;
 }
-return num/10;
+
+while(ch != 0){
+ a = a*10 + (ch % 2);
+ ch = ch / 2;
 }
+
+while (a != 0){
+    j = a % 10;
+    jj = jj * 10 + j;
+    a = a / 10;
+}
+
+if(y == 1){
+ jj = jj * 10;
+}
+return jj;
+}
+
